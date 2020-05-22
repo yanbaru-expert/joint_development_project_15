5614a0ccf38fae34129e5e9bb23d9e27a822bb86
浅野一輝 <asanokazuki@asanoikkinoMacBook-Pro.local>
2020-05-23 07:22:21 +0900

    newアクションとcreateアクションの追加

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,3 +1,12 @@
 class MessagesController < ApplicationController
-  resources :messages
+    def new
+      @message = Message.new
+    end
+    def create 
+      Message.create(message_params)
+    end
+    private
+    def message_params
+      params.require(:message).permit(:title,:content)
+    end
 end
